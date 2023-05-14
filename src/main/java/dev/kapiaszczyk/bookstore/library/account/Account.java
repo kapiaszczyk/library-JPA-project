8633eb08ad0d0f13d42766eb08fff683a1fabf69
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-14 14:15:27 +0200

    Add Account class

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/account/Account.java
@@ -0,0 +1,39 @@
+package dev.kapiaszczyk.bookstore.library.account;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.kapiaszczyk.bookstore.library.libraryUser.LibraryUser;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private String accountId;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "library_user_id", referencedColumnName = "library_user_id")
+    @JsonIgnore
+    private LibraryUser libraryUser;
+
+    public Account() {
+    }
+
+    public Account(LibraryUser libraryUser) {
+        this.libraryUser = libraryUser;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public LibraryUser getLibraryUser() {
+        return libraryUser;
+    }
+
+    public void setLibraryUser(LibraryUser libraryUser) {
+        this.libraryUser = libraryUser;
+    }
+}
