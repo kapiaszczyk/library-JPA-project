bd82fc2dccbc2cb6fd7a58901adf00d0445f0657
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-14 14:16:07 +0200

    Add Author class

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/author/Author.java
@@ -0,0 +1,46 @@
+package dev.kapiaszczyk.bookstore.library.author;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private String authorId;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @Column(name = "author_surname")
+    private String authorSurname;
+
+    public Author() {
+    }
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    public void setAuthorSurname(String authorSurname) {
+        this.authorSurname = authorSurname;
+    }
+}
