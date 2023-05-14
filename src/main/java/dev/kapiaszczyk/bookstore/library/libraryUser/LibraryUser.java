cd8baa2fa6703107843c694f501d2d32fb2e8e21
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-14 14:20:40 +0200

    Add LibraryUser class

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/libraryUser/LibraryUser.java
@@ -0,0 +1,60 @@
+package dev.kapiaszczyk.bookstore.library.libraryUser;
+
+import dev.kapiaszczyk.bookstore.library.account.Account;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "library_user")
+public class LibraryUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "library_user_id")
+    private String libraryUserId;
+
+    @Column(name = "library_user_name")
+    private String libraryUserName;
+
+    @Column(name = "library_user_surname")
+    private String libraryUserSurname;
+
+    @OneToOne(mappedBy = "libraryUser", cascade = CascadeType.ALL, orphanRemoval = true)
+    @PrimaryKeyJoinColumn
+    private Account account;
+
+    public LibraryUser() {
+    }
+
+    public LibraryUser(String libraryUserName, String libraryUserSurname) {
+        this.libraryUserName = libraryUserName;
+        this.libraryUserSurname = libraryUserSurname;
+    }
+
+    public String getLibraryUserId() {
+        return libraryUserId;
+    }
+
+    public String getLibraryUserName() {
+        return libraryUserName;
+    }
+
+    public void setLibraryUserName(String libraryUserName) {
+        this.libraryUserName = libraryUserName;
+    }
+
+    public String getLibraryUserSurname() {
+        return libraryUserSurname;
+    }
+
+    public void setLibraryUserSurname(String libraryUserSurname) {
+        this.libraryUserSurname = libraryUserSurname;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
