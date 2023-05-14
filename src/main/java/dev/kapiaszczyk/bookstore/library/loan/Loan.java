d944cc29fb2b47667553f049147f648a6dbe203b
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-14 14:20:48 +0200

    Add Loan class

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/loan/Loan.java
@@ -0,0 +1,25 @@
+package dev.kapiaszczyk.bookstore.library.loan;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "loan")
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "loan_id")
+    private String loanId;
+
+    // Relation with Account
+    @Column(name = "account_id")
+    private String accountId;
+
+    // Relation with Book
+    @Column(name = "book_id")
+    private String bookId;
+
+    @Column(name = "loan_status")
+    private String loanStatus;
+
+}
