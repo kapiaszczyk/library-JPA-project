57d1d099f9a33242fe4ac432853c724d051af82e
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-14 14:15:39 +0200

    Add Address class

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/address/Address.java
@@ -0,0 +1,47 @@
+package dev.kapiaszczyk.bookstore.library.address;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @Column(name = "address_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String addressId;
+
+    @Column(name = "street")
+    private String street;
+
+    // Relation with City
+    String cityId;
+
+    public Address() {
+    }
+
+    public Address(String street, String cityId) {
+        this.street = street;
+        this.cityId = cityId;
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+}
