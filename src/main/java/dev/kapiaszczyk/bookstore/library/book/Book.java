7448b53628b95e4a25bbdab2c2e31addf859d685
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-15 10:31:31 +0200

    Add associations to Inventory, Category, and Credit and update constructor, getters and setters

--- src/main/java/dev/kapiaszczyk/bookstore/library/book/Book.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/book/Book.java
@@ -1,6 +1,14 @@
 package dev.kapiaszczyk.bookstore.library.book;
 
+import dev.kapiaszczyk.bookstore.library.category.Category;
+import dev.kapiaszczyk.bookstore.library.credit.Credit;
+import dev.kapiaszczyk.bookstore.library.inventory.Inventory;
+import dev.kapiaszczyk.bookstore.library.isbn.ISBN;
+import dev.kapiaszczyk.bookstore.library.loan.Loan;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "book")
@@ -9,34 +17,49 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
-    private String bookId;
+    private Long bookId;
 
     @Column(name = "book_title")
     private String bookTitle;
 
-    // Relation with category
-    @Column(name = "category_id")
-    private String categoryId;
+    // Association with category
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
-    // Relation with isbn
-    @Column(name = "isbn_id")
-    private String isbnId;
+    // Association with isbn
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ISBN isbn;
 
-    // Relation with inventory
-    @Column(name = "inventory_id")
-    private String inventoryId;
+    // Association with inventory
+    @ManyToOne
+    private Inventory inventory;
+
+    // Association with credit
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Credit> credits = new ArrayList<>();
+
+    // Association with loan
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Loan loan;
+
+    // Constructors
 
     public Book() {
     }
 
-    public Book(String bookTitle, String categoryId, String isbnId, String inventoryId) {
+    public Book(String bookTitle, Category category, ISBN isbn, Inventory inventory, List<Credit> credits, Loan loan) {
         this.bookTitle = bookTitle;
-        this.categoryId = categoryId;
-        this.isbnId = isbnId;
-        this.inventoryId = inventoryId;
+        this.category = category;
+        this.isbn = isbn;
+        this.inventory = inventory;
+        this.credits = credits;
+        this.loan = loan;
     }
 
-    public String getBookId() {
+    // Getters and setters
+
+    public Long getBookId() {
         return bookId;
     }
 
@@ -48,27 +71,43 @@ public class Book {
         this.bookTitle = bookTitle;
     }
 
-    public String getCategoryId() {
-        return categoryId;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
-    public String getIsbnId() {
-        return isbnId;
+    public ISBN getIsbn() {
+        return isbn;
     }
 
-    public void setIsbnId(String isbnId) {
-        this.isbnId = isbnId;
+    public void setIsbn(ISBN isbn) {
+        this.isbn = isbn;
     }
 
-    public String getInventoryId() {
-        return inventoryId;
+    public Inventory getInventory() {
+        return inventory;
     }
 
-    public void setInventoryId(String inventoryId) {
-        this.inventoryId = inventoryId;
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public List<Credit> getCredits() {
+        return credits;
+    }
+
+    public void setCredits(List<Credit> credits) {
+        this.credits = credits;
+    }
+
+    public Loan getLoan() {
+        return loan;
+    }
+
+    public void setLoan(Loan loan) {
+        this.loan = loan;
     }
 }
