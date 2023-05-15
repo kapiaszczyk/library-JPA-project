3cf04fa2346937cf2f3b03e8f5107165821d9256
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-15 20:21:55 +0200

    Add association with Book

--- src/main/java/dev/kapiaszczyk/bookstore/library/category/Category.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/category/Category.java
@@ -1,6 +1,10 @@
 package dev.kapiaszczyk.bookstore.library.category;
 
+import dev.kapiaszczyk.bookstore.library.book.Book;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -13,6 +17,10 @@ public class Category {
 
     @Column(name = "category_name")
     private String categoryName;
+
+    // Association with book
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
 
     public Category() {
     }
@@ -31,5 +39,21 @@ public class Category {
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        this.books.remove(book);
     }
 }
