2ef1d85f265cd9cdcfc06f43116e6de2801e5ff3
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-15 19:57:24 +0200

    Change LibraryUser id to be type Long

--- src/main/java/dev/kapiaszczyk/bookstore/library/libraryUser/LibraryUserRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/libraryUser/LibraryUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface LibraryUserRepository extends JpaRepository<LibraryUser, String> {
+public interface LibraryUserRepository extends JpaRepository<LibraryUser, Long> {
     List<LibraryUser> findAll();
 }
