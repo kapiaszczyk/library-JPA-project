f9c2f1ffec53396c4d326e6cad047e62cfaff23a
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-15 20:38:29 +0200

    Add association with Library and Book

--- src/main/java/dev/kapiaszczyk/bookstore/library/inventory/Inventory.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/inventory/Inventory.java
@@ -1,6 +1,11 @@
 package dev.kapiaszczyk.bookstore.library.inventory;
 
+import dev.kapiaszczyk.bookstore.library.book.Book;
+import dev.kapiaszczyk.bookstore.library.library.Library;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "inventory")
@@ -9,12 +14,41 @@ public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "inventory_id")
-    private String inventoryId;
+    private Long inventoryId;
+
+    // Association with book
+    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Book> book = new ArrayList<>();
+
+    // Association with library
+    @OneToOne
+    private Library library;
 
     public Inventory() {
     }
 
-    public String getInventoryId() {
+    public Inventory(List<Book> book, Library library) {
+        this.book = book;
+        this.library = library;
+    }
+
+    public Long getInventoryId() {
         return inventoryId;
+    }
+
+    public List<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Book> book) {
+        this.book = book;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
     }
 }
