5a73e60058a1ded3ba1447b23b31c5811886c356
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-15 19:44:53 +0200

    Change authorId field to be Long type

--- src/main/java/dev/kapiaszczyk/bookstore/library/author/Author.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/author/Author.java
@@ -8,7 +8,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "author_id")
-    private String authorId;
+    private Long authorId;
 
     @Column(name = "author_name")
     private String authorName;
@@ -24,7 +24,7 @@ public class Author {
         this.authorSurname = authorSurname;
     }
 
-    public String getAuthorId() {
+    public Long getAuthorId() {
         return authorId;
     }
 
