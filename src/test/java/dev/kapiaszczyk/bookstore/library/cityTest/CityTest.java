a54fa091095b784eafa85469d33639eed682256b
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:57:16 +0200

    Add Transactional annotation

--- src/test/java/dev/kapiaszczyk/bookstore/library/cityTest/CityTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/cityTest/CityTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Transactional
 public class CityTest {
 
     @Autowired
