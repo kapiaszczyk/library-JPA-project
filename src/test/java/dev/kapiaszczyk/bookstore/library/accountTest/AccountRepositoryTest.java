5650b9b76dd5b5885d8c4ed381933f8a826501a4
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 19:19:53 +0200

    Add AccountRepositoryTest for testing repository methods

---
+++ src/test/java/dev/kapiaszczyk/bookstore/library/accountTest/AccountRepositoryTest.java
@@ -0,0 +1,65 @@
+package dev.kapiaszczyk.bookstore.library.accountTest;
+
+import dev.kapiaszczyk.bookstore.library.account.Account;
+import dev.kapiaszczyk.bookstore.library.account.AccountRepository;
+import dev.kapiaszczyk.bookstore.library.libraryUser.LibraryUser;
+import dev.kapiaszczyk.bookstore.library.libraryUser.LibraryUserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private LibraryUserRepository libraryUserRepository;
+
+    private Account account;
+    private LibraryUser libraryUser;
+
+    @BeforeEach
+    public void setUp() {
+        account = new Account();
+        account.setAccountNumber("123456789");
+
+        libraryUser = new LibraryUser();
+        libraryUser.setLibraryUserFirstName("John");
+        libraryUser.setLibraryUserSurname("Doe");
+
+        account.setLibraryUser(libraryUser);
+        libraryUser.setAccount(account);
+
+        libraryUserRepository.save(libraryUser);
+        accountRepository.save(account);
+
+    }
+
+    @Test
+    public void findAccountByLibraryUserLibraryUserId() {
+        // Retrieve account
+        Account savedAccount = accountRepository.findByLibraryUserLibraryUserId(libraryUser.getLibraryUserId());
+
+        // Check if account can be found by libraryUserId
+        assertNotNull(savedAccount.getAccountId());
+        assertThat(savedAccount.getAccountNumber(), equalTo(account.getAccountNumber()));
+        assertThat(savedAccount.getLibraryUser().getLibraryUserId(), equalTo(libraryUser.getLibraryUserId()));
+    }
+
+}
+
+
