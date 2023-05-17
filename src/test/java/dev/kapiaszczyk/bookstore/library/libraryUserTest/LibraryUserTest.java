d9a63ec167b38b9fba86182cabd6da7af06bff67
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:58:19 +0200

    Refactor LibraryUserTest for better readability and remove JUnit4 annotations

--- src/test/java/dev/kapiaszczyk/bookstore/library/libraryUserTest/LibraryUserTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/libraryUserTest/LibraryUserTest.java
@@ -3,19 +3,22 @@ package dev.kapiaszczyk.bookstore.library.libraryUserTest;
 import dev.kapiaszczyk.bookstore.library.account.Account;
 import dev.kapiaszczyk.bookstore.library.libraryUser.LibraryUser;
 import dev.kapiaszczyk.bookstore.library.libraryUser.LibraryUserRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class LibraryUserTest {
@@ -23,41 +26,36 @@ public class LibraryUserTest {
     @Autowired
     private LibraryUserRepository libraryUserRepository;
 
-    @Test
-    public void saveLibraryUser() {
-        LibraryUser libraryUser = new LibraryUser();
+    private LibraryUser libraryUser;
+    private Account account;
+
+    @BeforeEach
+    public void setUp() {
+        libraryUser = new LibraryUser();
         libraryUser.setLibraryUserFirstName("John");
         libraryUser.setLibraryUserSurname("Doe");
 
-        Account account = new Account();
+        account = new Account();
         account.setAccountNumber("123456789");
 
         libraryUser.setAccount(account);
         account.setLibraryUser(libraryUser);
         libraryUserRepository.save(libraryUser);
-
-        LibraryUser savedLibraryUser = libraryUserRepository.findById(libraryUser.getLibraryUserId()).get();
-
-        assertNotNull(savedLibraryUser.getLibraryUserId());
-        assertEquals("John", savedLibraryUser.getLibraryUserFirstName());
-        assertEquals("Doe", savedLibraryUser.getLibraryUserSurname());
-        assertEquals("123456789", savedLibraryUser.getAccount().getAccountNumber());
-        assertNotNull(savedLibraryUser.getAccount().getAccountId());
     }
 
     @Test
-    public void updateLibraryUser() {
-        LibraryUser libraryUser = new LibraryUser();
-        libraryUser.setLibraryUserFirstName("John");
-        libraryUser.setLibraryUserSurname("Doe");
+    public void libraryUserCanBeAdded() {
+        LibraryUser savedLibraryUser = libraryUserRepository.findById(libraryUser.getLibraryUserId()).get();
 
-        Account account = new Account();
-        account.setAccountNumber("123456789");
-        libraryUser.setAccount(account);
-        account.setLibraryUser(libraryUser);
+        assertNotNull(savedLibraryUser.getLibraryUserId());
+        assertThat(savedLibraryUser.getLibraryUserFirstName(), equalTo(libraryUser.getLibraryUserFirstName()));
+        assertThat(savedLibraryUser.getLibraryUserSurname(), equalTo(libraryUser.getLibraryUserSurname()));
+        assertThat(savedLibraryUser.getAccount().getAccountNumber(), equalTo(libraryUser.getAccount().getAccountNumber()));
 
-        libraryUserRepository.save(libraryUser);
+    }
 
+    @Test
+    public void libraryUserCanBeUpdated() {
         LibraryUser savedLibraryUser = libraryUserRepository.findById(libraryUser.getLibraryUserId()).get();
 
         savedLibraryUser.setLibraryUserFirstName("Jane");
@@ -67,8 +65,15 @@ public class LibraryUserTest {
 
         LibraryUser updatedLibraryUser = libraryUserRepository.findById(savedLibraryUser.getLibraryUserId()).get();
 
-        assertEquals("Jane", updatedLibraryUser.getLibraryUserFirstName());
-        assertEquals("Smith", updatedLibraryUser.getLibraryUserSurname());
+        assertThat(updatedLibraryUser.getLibraryUserFirstName(), equalTo(savedLibraryUser.getLibraryUserFirstName()));
+        assertThat(updatedLibraryUser.getLibraryUserSurname(), equalTo(savedLibraryUser.getLibraryUserSurname()));
+    }
+
+    @Test
+    public void libraryUserCanBeDeleted() {
+        LibraryUser savedLibraryUser = libraryUserRepository.findById(libraryUser.getLibraryUserId()).get();
+        libraryUserRepository.delete(savedLibraryUser);
+        assertEquals(0, libraryUserRepository.count());
     }
 
 }
