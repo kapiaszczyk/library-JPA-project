3a469a1f29598e3a1438fae20222fa4357527b1d
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:15:45 +0200

    Refactor AuthorTest for better readability and remove JUnit4 annotations

--- src/test/java/dev/kapiaszczyk/bookstore/library/bookTest/BookTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/bookTest/BookTest.java
@@ -8,19 +8,22 @@ import dev.kapiaszczyk.bookstore.library.credit.Credit;
 import dev.kapiaszczyk.bookstore.library.credit.CreditRepository;
 import dev.kapiaszczyk.bookstore.library.isbn.ISBN;
 import dev.kapiaszczyk.bookstore.library.isbn.ISBNRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class BookTest {
@@ -39,8 +42,8 @@ public class BookTest {
 
     private Book book;
 
-    @Test
-    public void saveBook() {
+    @BeforeEach
+    private void setUp() {
         book = new Book();
         book.setBookTitle("Test title");
 
@@ -66,81 +69,32 @@ public class BookTest {
         book.addCredit(credit);
 
         bookRepository.save(book);
+    }
 
+    @Test
+    public void saveBook() {
         Book savedBook = bookRepository.findById(book.getBookId()).get();
 
         assertNotNull(book);
-        assertEquals("Test title", savedBook.getBookTitle());
-        assertEquals("123456789L", savedBook.getIsbn().getIsbnNumber());
-        assertEquals("Test author", savedBook.getCredits().get(0).getAuthor().getAuthorName());
-        assertEquals("Test surname", savedBook.getCredits().get(0).getAuthor().getAuthorSurname());
+        assertThat(savedBook.getBookId(), equalTo(book.getBookId()));
+        assertThat(savedBook.getBookTitle(), equalTo(book.getBookTitle()));
+        assertThat(savedBook.getIsbn().getIsbnNumber(), equalTo(book.getIsbn().getIsbnNumber()));
+        assertThat(savedBook.getCredits().get(0).getAuthor().getAuthorName(), equalTo(book.getCredits().get(0).getAuthor().getAuthorName()));
+        assertThat(savedBook.getCredits().get(0).getAuthor().getAuthorSurname(), equalTo(book.getCredits().get(0).getAuthor().getAuthorSurname()));
     }
 
     @Test
     public void updateBook() {
-        book = new Book();
-        book.setBookTitle("Test title");
-
-        ISBN isbn = new ISBN();
-        isbn.setIsbnNumber("123456789L");
-        isbn.setBook(book);
-        book.setIsbn(isbn);
-
-        Credit credit = new Credit();
-        credit.setBook(book);
-
-        Author author = new Author();
-        author.setAuthorName("Test author");
-        author.setAuthorSurname("Test surname");
-
-        credit.setAuthor(author);
-        author.addCredit(credit);
-
-        isbnRepository.save(isbn);
-        authorRepository.save(author);
-        creditRepository.save(credit);
-
-        book.addCredit(credit);
-
-        bookRepository.save(book);
-
         Book savedBook = bookRepository.findById(book.getBookId()).get();
-
         savedBook.setBookTitle("Updated title");
         bookRepository.save(savedBook);
 
         Book updatedBook = bookRepository.findById(book.getBookId()).get();
-        assertEquals("Updated title", updatedBook.getBookTitle());
+        assertThat(updatedBook.getBookTitle(), equalTo(savedBook.getBookTitle()));
     }
 
     @Test
     public void deleteBook() {
-        book = new Book();
-        book.setBookTitle("Test title");
-
-        ISBN isbn = new ISBN();
-        isbn.setIsbnNumber("123456789L");
-        isbn.setBook(book);
-        book.setIsbn(isbn);
-
-        Credit credit = new Credit();
-        credit.setBook(book);
-
-        Author author = new Author();
-        author.setAuthorName("Test author");
-        author.setAuthorSurname("Test surname");
-
-        credit.setAuthor(author);
-        author.addCredit(credit);
-
-        isbnRepository.save(isbn);
-        authorRepository.save(author);
-        creditRepository.save(credit);
-
-        book.addCredit(credit);
-
-        bookRepository.save(book);
-
         Book savedBook = bookRepository.findById(book.getBookId()).get();
         isbnRepository.delete(savedBook.getIsbn());
         authorRepository.delete(savedBook.getCredits().get(0).getAuthor());
@@ -148,8 +102,6 @@ public class BookTest {
         bookRepository.delete(savedBook);
 
         assertEquals(0, bookRepository.count());
-
-
     }
 
 }
