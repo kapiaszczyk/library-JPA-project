134157309cae79b5f5595e524c0b4f917de11211
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:57:50 +0200

    Refactor LibraryTest for better readability and remove JUnit4 annotations

--- src/test/java/dev/kapiaszczyk/bookstore/library/libraryTest/LibraryTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/libraryTest/LibraryTest.java
@@ -3,19 +3,22 @@ package dev.kapiaszczyk.bookstore.library.libraryTest;
 import dev.kapiaszczyk.bookstore.library.inventory.Inventory;
 import dev.kapiaszczyk.bookstore.library.library.Library;
 import dev.kapiaszczyk.bookstore.library.library.LibraryRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class LibraryTest {
@@ -23,60 +26,45 @@ public class LibraryTest {
     @Autowired
     private LibraryRepository libraryRepository;
 
-    @Test
-    public void addLibrary() {
+    private Library library;
+    private Inventory inventory;
 
-        Library library = new Library();
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
         library.setLibraryName("Warsaw Library");
 
-
-        Inventory inventory = new Inventory();
+        inventory = new Inventory();
         inventory.setLibrary(library);
         library.setInventory(inventory);
 
         libraryRepository.save(library);
+    }
 
+    @Test
+    public void libraryCanBeAdded() {
         Library savedLibrary = libraryRepository.findById(library.getLibraryId()).get();
 
         assertNotNull(savedLibrary.getLibraryId());
-        assertEquals("Warsaw Library", savedLibrary.getLibraryName());
-        assertEquals(library.getInventory().getInventoryId(), savedLibrary.getInventory().getInventoryId());
+        assertThat(savedLibrary.getLibraryName(), equalTo(library.getLibraryName()));
+        assertThat(savedLibrary.getInventory().getInventoryId(), equalTo(inventory.getInventoryId()));
 
     }
 
     @Test
-    public void updateLibrary() {
-        Library library = new Library();
-        library.setLibraryName("Warsaw Library");
-
-        Inventory inventory = new Inventory();
-        inventory.setLibrary(library);
-        library.setInventory(inventory);
-
-        libraryRepository.save(library);
-
+    public void libraryCanBeUpdated() {
         Library savedLibrary = libraryRepository.findById(library.getLibraryId()).get();
 
         savedLibrary.setLibraryName("Cracow Library");
-
         libraryRepository.save(savedLibrary);
 
         Library updatedLibrary = libraryRepository.findById(library.getLibraryId()).get();
-        assertEquals("Cracow Library", updatedLibrary.getLibraryName());
+        assertThat(updatedLibrary.getLibraryName(), equalTo(savedLibrary.getLibraryName()));
 
     }
 
     @Test
-    public void deleteLibrary() {
-        Library library = new Library();
-        library.setLibraryName("Warsaw Library");
-
-        Inventory inventory = new Inventory();
-        inventory.setLibrary(library);
-        library.setInventory(inventory);
-
-        libraryRepository.save(library);
-
+    public void libraryCanBeDeleted() {
         Library savedLibrary = libraryRepository.findById(library.getLibraryId()).get();
 
         libraryRepository.delete(savedLibrary);
