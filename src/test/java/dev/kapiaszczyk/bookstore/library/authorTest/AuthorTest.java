2bc44f8f4ad0080c4ba3346e0153b5bc50a9b871
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:10:59 +0200

    Refactor AuthorTest for better readability and remove JUnit4 annotations

--- src/test/java/dev/kapiaszczyk/bookstore/library/authorTest/AuthorTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/authorTest/AuthorTest.java
@@ -3,18 +3,21 @@ package dev.kapiaszczyk.bookstore.library.authorTest;
 import dev.kapiaszczyk.bookstore.library.author.Author;
 import dev.kapiaszczyk.bookstore.library.author.AuthorRepository;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class AuthorTest {
@@ -24,27 +27,22 @@ public class AuthorTest {
 
     private Author author;
 
-    @Test
-    public void saveAuthor() {
+    @BeforeEach
+    public void setUp() {
         author = new Author();
         author.setAuthorName("Test name");
         author.setAuthorSurname("Test surname");
         authorRepository.save(author);
-
-        Author savedAuthor = authorRepository.findById(author.getAuthorId()).get();
-
-        assertNotNull(savedAuthor);
-        assertEquals(author.getAuthorName(), savedAuthor.getAuthorName());
-        assertEquals(author.getAuthorSurname(), savedAuthor.getAuthorSurname());
     }
 
     @Test
-    public void updateAuthor() {
-        author = new Author();
-        author.setAuthorName("Test name");
-        author.setAuthorSurname("Test surname");
-        authorRepository.save(author);
+    public void authorCanBeAdded() {
+        Author savedAuthor = authorRepository.findById(author.getAuthorId()).get();
+        assertNotNull(savedAuthor);
+    }
 
+    @Test
+    public void authorCanBeUpdated() {
         Author savedAuthor = authorRepository.findById(author.getAuthorId()).get();
 
         savedAuthor.setAuthorName("Updated name");
@@ -52,17 +50,12 @@ public class AuthorTest {
         authorRepository.save(savedAuthor);
 
         Author updatedAuthor = authorRepository.findById(savedAuthor.getAuthorId()).get();
-        assertEquals(savedAuthor.getAuthorName(), updatedAuthor.getAuthorName());
-        assertEquals(savedAuthor.getAuthorSurname(), updatedAuthor.getAuthorSurname());
+        assertThat(updatedAuthor.getAuthorName(), equalTo(savedAuthor.getAuthorName()));
+        assertThat(updatedAuthor.getAuthorSurname(), equalTo(savedAuthor.getAuthorSurname()));
     }
 
     @Test
-    public void deleteAuthor() {
-        author = new Author();
-        author.setAuthorName("Test name");
-        author.setAuthorSurname("Test surname");
-        authorRepository.save(author);
-
+    public void authorCanBeDeleted() {
         Author savedAuthor = authorRepository.findById(author.getAuthorId()).get();
         authorRepository.delete(savedAuthor);
 
