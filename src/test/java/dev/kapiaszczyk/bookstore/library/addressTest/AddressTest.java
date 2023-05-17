8dce29f95350962df2a195c8975132d4b85eb9ab
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:02:29 +0200

    Refactor AddressTest for better readability by using BeforeEach setup

--- src/test/java/dev/kapiaszczyk/bookstore/library/addressTest/AddressTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/addressTest/AddressTest.java
@@ -5,12 +5,14 @@ import dev.kapiaszczyk.bookstore.library.address.AddressRepository;
 import dev.kapiaszczyk.bookstore.library.city.City;
 import dev.kapiaszczyk.bookstore.library.city.CityRepository;
 import dev.kapiaszczyk.bookstore.library.cityCode.CityCode;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
@@ -19,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class AddressTest {
@@ -34,27 +36,34 @@ public class AddressTest {
     private City city;
     private CityCode cityCode;
 
-    @Test
-    public void addAddressTest() {
+    @BeforeEach
+    public void setUp() {
+
         city = new City("Warsaw");
         cityCode = new CityCode("00-000");
 
         cityCode.setCity(city);
         city.setCityCode(Collections.singletonList(cityCode));
 
+
+        address = new Address();
+        address.setStreet("St. Mary's Street 5");
+
         cityRepository.save(city);
 
         address = new Address();
         address.setStreet("St. Mary's Street 5");
-        address.setCity(cityRepository.findById(city.getCityId()).get()); // set the city field
+        address.setCity(city);
 
         addressRepository.save(address);
+    }
 
-        Long addressId = address.getAddressId();
-        assertNotNull(addressId);
+    @Test
+    public void addressCanBeAdded() {
+        // Retrieve address
+        Address savedAddress = addressRepository.findById(address.getAddressId()).get();
 
-        Address savedAddress = addressRepository.findById(addressId).get();
-
+        // Check if address was added
         assertNotNull(savedAddress);
         assertNotNull(savedAddress.getAddressId());
         assertEquals("St. Mary's Street 5", savedAddress.getStreet());
@@ -63,23 +72,7 @@ public class AddressTest {
     }
 
     @Test
-    public void updateAddressTest() {
-
-        // Create address
-        city = new City("Warsaw");
-        cityCode = new CityCode("00-000");
-
-        cityCode.setCity(city);
-        city.setCityCode(Collections.singletonList(cityCode));
-
-        cityRepository.save(city);
-
-        address = new Address();
-        address.setStreet("St. Mary's Street 5");
-        address.setCity(cityRepository.findById(city.getCityId()).get());
-
-        addressRepository.save(address);
-
+    public void addressCanBeUpdated() {
         // Update address
         Address savedAddress = addressRepository.findById(address.getAddressId()).get();
         savedAddress.setStreet("St. Mary's Street 6");
@@ -91,24 +84,8 @@ public class AddressTest {
     }
 
     @Test
-    public void deleteAddress() {
-        // Create address
-        city = new City("Warsaw");
-        cityCode = new CityCode("00-000");
-
-        cityCode.setCity(city);
-        city.setCityCode(Collections.singletonList(cityCode));
-
-        cityRepository.save(city);
-
-        address = new Address();
-        address.setStreet("St. Mary's Street 5");
-        address.setCity(cityRepository.findById(city.getCityId()).get());
-
-        addressRepository.save(address);
-
+    public void addressCanBeDeleted() {
         // Delete address
-
         addressRepository.delete(address);
 
         // Check if address was deleted
