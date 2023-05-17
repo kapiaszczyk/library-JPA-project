78b9369f0dd9218b4274dd3268f263a067fdb141
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-17 18:34:06 +0200

    Refactor CityCodeTest for better readability and remove JUnit4 annotations

--- src/test/java/dev/kapiaszczyk/bookstore/library/cityCode/CityCodeTest.java
+++ src/test/java/dev/kapiaszczyk/bookstore/library/cityCode/CityCodeTest.java
@@ -1,17 +1,20 @@
 package dev.kapiaszczyk.bookstore.library.cityCode;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 public class CityCodeTest {
@@ -19,26 +22,33 @@ public class CityCodeTest {
     @Autowired
     private CityCodeRepository cityRepository;
 
-    @Test
-    public void saveCityCode() {
-        CityCode cityCode = new CityCode("00-000");
+    private CityCode cityCode;
+
+    @BeforeEach
+    public void setUp() {
+        cityCode = new CityCode("00-000");
         cityRepository.save(cityCode);
-        assertNotNull(cityCode);
     }
 
     @Test
-    public void updateCityCode() {
-        CityCode cityCode = new CityCode("00-000");
-        cityRepository.save(cityCode);
+    public void cityCodeCanBeAdded() {
+        CityCode savedCityCode = cityRepository.findById(cityCode.getCityCodeId()).get();
+        assertNotNull(savedCityCode);
+    }
+
+    @Test
+    public void cityCodeCanBeUpdated() {
+        CityCode savedCityCode = cityRepository.findById(cityCode.getCityCodeId()).get();
         cityCode.setCityCode("11-111");
         cityRepository.save(cityCode);
-        assertNotNull(cityCode);
+
+        CityCode updatedCityCode = cityRepository.findById(cityCode.getCityCodeId()).get();
+        assertNotNull(updatedCityCode);
+        assertThat(savedCityCode.getCityCodeId(), equalTo(updatedCityCode.getCityCodeId()));
     }
 
     @Test
-    public void deleteCityCode() {
-        CityCode cityCode = new CityCode("00-000");
-        cityRepository.save(cityCode);
+    public void cityCodeCanBeDeleted() {
         cityRepository.delete(cityCode);
     }
 
