659f76492a680300648626c78c105e6e4682570a
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-18 13:10:36 +0200

    Add findByBookTitle query method to BookRepository with test case
    Add a new query method, findByBookTitle, to the BookRepository interface. The method enables retrieving a list of authors based on their surname. Additionally, a corresponding test case has been added.

--- src/main/java/dev/kapiaszczyk/bookstore/library/book/BookRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/book/BookRepository.java
@@ -3,6 +3,12 @@ package dev.kapiaszczyk.bookstore.library.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<List<Book>> findByBookTitle(String bookTitle);
+
 }
---
+++ src/test/java/dev/kapiaszczyk/bookstore/library/bookTest/BookRepositoryTest.java
@@ -0,0 +1,92 @@
+package dev.kapiaszczyk.bookstore.library.bookTest;
+
+import dev.kapiaszczyk.bookstore.library.author.Author;
+import dev.kapiaszczyk.bookstore.library.author.AuthorRepository;
+import dev.kapiaszczyk.bookstore.library.book.Book;
+import dev.kapiaszczyk.bookstore.library.book.BookRepository;
+import dev.kapiaszczyk.bookstore.library.credit.Credit;
+import dev.kapiaszczyk.bookstore.library.credit.CreditRepository;
+import dev.kapiaszczyk.bookstore.library.isbn.ISBN;
+import dev.kapiaszczyk.bookstore.library.isbn.ISBNRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private CreditRepository creditRepository;
+
+    @Autowired
+    private ISBNRepository isbnRepository;
+
+    private Book book;
+    private ISBN isbn;
+    private Credit credit;
+    private Author author;
+
+    @BeforeEach
+    public void setUp() {
+        book = new Book();
+        book.setBookTitle("Hamlet");
+
+        isbn = new ISBN();
+        isbn.setIsbnNumber("123456789L");
+        isbn.setBook(book);
+        book.setIsbn(isbn);
+
+        credit = new Credit();
+        credit.setBook(book);
+
+        author = new Author();
+        author.setAuthorName("William");
+        author.setAuthorSurname("Shakespeare");
+
+        credit.setAuthor(author);
+        author.addCredit(credit);
+
+        isbnRepository.save(isbn);
+        authorRepository.save(author);
+        creditRepository.save(credit);
+
+        book.addCredit(credit);
+
+        bookRepository.save(book);
+    }
+
+    @Test
+    public void shouldFindBookByTitle() {
+        String title = "Hamlet";
+        Optional<List<Book>> optionalBooks = bookRepository.findByBookTitle(title);
+
+        assertTrue(optionalBooks.isPresent());
+
+        List<Book> books = optionalBooks.get();
+
+        assertThat(books.size(), equalTo(1));
+        assertThat(books.get(0).getBookTitle(), equalTo(book.getBookTitle()));
+    }
+
+}
