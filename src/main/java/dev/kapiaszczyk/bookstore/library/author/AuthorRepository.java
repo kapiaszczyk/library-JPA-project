9cd6e001a26c8c85f5b22241152aee5d3b7bd605
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-18 12:44:13 +0200

    Add findByAuthorName custom query method to find Authors by their name

--- src/main/java/dev/kapiaszczyk/bookstore/library/author/AuthorRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/author/AuthorRepository.java
@@ -3,6 +3,10 @@ package dev.kapiaszczyk.bookstore.library.author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<List<Author>> findByAuthorName(String authorName);
 }
