c98f7e65f01037f48038c942f9b5096182ebb98b
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-18 13:25:18 +0200

    Add findByCategoryName query method to CategoryRepository with test case
    Add a new query method, findByCategoryName, to the CategoryRepository interface to enable category based on given name. Additionally, a corresponding test case has been added.

--- src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryRepository.java
@@ -3,6 +3,11 @@ package dev.kapiaszczyk.bookstore.library.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByCategoryName(String categoryName);
+
 }
---
+++ src/test/java/dev/kapiaszczyk/bookstore/library/categoryTest/CategoryRepositoryTest.java
@@ -0,0 +1,49 @@
+package dev.kapiaszczyk.bookstore.library.categoryTest;
+
+import dev.kapiaszczyk.bookstore.library.category.Category;
+import dev.kapiaszczyk.bookstore.library.category.CategoryRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    private Category category;
+
+    @BeforeEach
+    public void setUp() {
+        category = new Category();
+        category.setCategoryName("Fantasy");
+        categoryRepository.save(category);
+    }
+
+    @Test
+    public void shouldFindCategoryByName() {
+        Optional<Category> optionalCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+
+        assertTrue(optionalCategory.isPresent());
+        Category foundCategory = optionalCategory.get();
+
+        assertNotNull(foundCategory);
+        assertThat(foundCategory.getCategoryName(), equalTo(category.getCategoryName()));
+    }
+
+}
