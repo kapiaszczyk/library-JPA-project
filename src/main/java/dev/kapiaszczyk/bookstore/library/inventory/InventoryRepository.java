4d07c322800b653e4481cdc5a2a27a2b470e0be5
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-18 13:44:26 +0200

    Add findByLibraryLibraryName query method to InventoryRepository with test case
    Add a new query method, findByLibraryLibraryName, to the InventoryRepository interface to enable retrieving inventory given a library name. Additionally, a corresponding test case has been added.

--- src/main/java/dev/kapiaszczyk/bookstore/library/inventory/InventoryRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/inventory/InventoryRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Inventory findByLibraryLibraryName(String libraryName);
 }
---
+++ src/test/java/dev/kapiaszczyk/bookstore/library/inventoryTest/InventoryRepositoryTest.java
@@ -0,0 +1,60 @@
+package dev.kapiaszczyk.bookstore.library.inventoryTest;
+
+import dev.kapiaszczyk.bookstore.library.city.City;
+import dev.kapiaszczyk.bookstore.library.city.CityRepository;
+import dev.kapiaszczyk.bookstore.library.cityCode.CityCode;
+import dev.kapiaszczyk.bookstore.library.cityCode.CityCodeRepository;
+import dev.kapiaszczyk.bookstore.library.inventory.Inventory;
+import dev.kapiaszczyk.bookstore.library.inventory.InventoryRepository;
+import dev.kapiaszczyk.bookstore.library.library.Library;
+import dev.kapiaszczyk.bookstore.library.library.LibraryRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class InventoryRepositoryTest {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Autowired
+    private LibraryRepository libraryRepository;
+
+    private Inventory inventory;
+    private Library library;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+        library.setLibraryName("London Library");
+
+        inventory = new Inventory();
+        inventory.setLibrary(library);
+        library.setInventory(inventory);
+
+        libraryRepository.save(library);
+    }
+
+    @Test
+    public void shouldFindByLibraryName() {
+        Inventory savedInventory = inventoryRepository.findByLibraryLibraryName(library.getLibraryName());
+
+        assertNotNull(savedInventory.getInventoryId());
+        assertThat(savedInventory.getLibrary().getLibraryName(), equalTo(library.getLibraryName()));
+    }
+
+}
