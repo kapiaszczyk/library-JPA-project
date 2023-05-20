272ed3d578dc1898c907a5abfefc35f662de8a6b
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-20 19:23:20 +0200

    Add `AuthorDTO` class along with `AuthorMapper`

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/author/AuthorMapper.java
@@ -0,0 +1,22 @@
+package dev.kapiaszczyk.bookstore.library.author;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AuthorMapper {
+
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    AuthorDTO mapToAuthorDTO(Author author);
+
+    @Named("mapToAuthorDTOList")
+    List<AuthorDTO> mapToAuthorDTOList(List<Author> authors);
+}
