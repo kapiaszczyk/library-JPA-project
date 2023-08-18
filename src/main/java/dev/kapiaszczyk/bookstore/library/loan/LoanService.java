9374a481a070c89b1eacb815eccf62b5d47457eb
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:23:26 +0200

    Add LoanService

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/loan/LoanService.java
@@ -0,0 +1,42 @@
+package dev.kapiaszczyk.bookstore.library.loan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanService {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+//    // TODO: Add loan
+//    public Loan addLoan(Loan loan) {
+//        return loanRepository.save(loan);
+//    }
+
+//    // TODO: Delete loan
+//    public void deleteLoan(Long id) {
+//        loanRepository.deleteById(id);
+//    }
+
+//    // TODO: Update loan
+//    public Loan updateLoan(Loan loan) {
+//        return loanRepository.save(loan);
+//    }
+
+    // TODO: View all loans with account id
+    public List<Loan> viewAllLoansWithAccountId(Long id) {
+        return loanRepository.findByAccountId(id);
+    }
+
+    // TODO: View all loans with status
+    public Iterable<Loan> viewAllLoansWithStatus(Loan.Status status) { return loanRepository.findByStatus(status);
+    }
+
+    // TODO: View all loans with account number and status
+    public List<Loan> viewAllLoansWithAccountNumberAndStatus(String accountNumber, Loan.Status status) {
+        return loanRepository.findByAccountNumberAndStatus(accountNumber, status);
+    }
+}
