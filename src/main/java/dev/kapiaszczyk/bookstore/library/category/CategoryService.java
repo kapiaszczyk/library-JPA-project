37d5352972b771a2da1bb50611c85d2a7b8c2749
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:21:22 +0200

    Add CRUD functionality to CategoryService

--- src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryService.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryService.java
@@ -3,6 +3,7 @@ package dev.kapiaszczyk.bookstore.library.category;
 import dev.kapiaszczyk.bookstore.library.category.dto.CategoryMapper;
 import dev.kapiaszczyk.bookstore.library.category.dto.CategoryStatisticsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,4 +28,16 @@ public class CategoryService {
         return CategoryMapper.mapToDTOList(results);
     }
 
+    public Object save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Object deleteById(Long id) {
+        categoryRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
 }
