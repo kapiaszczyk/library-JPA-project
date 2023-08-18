d79f948677bbb4e61b86d9ac8a8bb71ff9892d77
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:22:29 +0200

    Add LoanController with basic CRUD functionality

---
+++ src/main/java/dev/kapiaszczyk/bookstore/library/loan/LoanController.java
@@ -0,0 +1,48 @@
+package dev.kapiaszczyk.bookstore.library.loan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoanController {
+
+    @Autowired
+    private LoanService loanService;
+
+//    // TODO: Add loan
+//    @PostMapping("/loans/add")
+//    public Loan addLoan(Loan loan) {
+//        return loanService.addLoan(loan);
+//    }
+
+//    // TODO: Delete loan
+//    @DeleteMapping("/loans/delete")
+//    public void deleteLoan(@RequestParam("id") Long id) {
+//        loanService.deleteLoan(id);
+//    }
+
+//    // TODO: Update loan
+//    @PutMapping("/loans/update")
+//    public Loan updateLoan(Loan loan) {
+//        return loanService.updateLoan(loan);
+//    }
+
+    // TODO: View all loans with account id
+    @GetMapping("/loans/account")
+    public Iterable<Loan> viewAllLoansWithAccountId(@RequestParam("id") Long id) {
+        return loanService.viewAllLoansWithAccountId(id);
+    }
+
+    // TODO: View all loans with status
+    @GetMapping("/loans/status")
+    public Iterable<Loan> viewAllLoansWithStatus(@RequestParam("status") Loan.Status status) {
+        return loanService.viewAllLoansWithStatus(status);
+    }
+
+    // TODO: View all loans with account and status
+    @GetMapping("/loans/account/status")
+    public Iterable<Loan> viewAllLoansWithAccountNumberAndStatus(@RequestParam("accountNumber") String accountNumber, @RequestParam("status") Loan.Status status) {
+        return loanService.viewAllLoansWithAccountNumberAndStatus(accountNumber, status);
+    }
+
+}
