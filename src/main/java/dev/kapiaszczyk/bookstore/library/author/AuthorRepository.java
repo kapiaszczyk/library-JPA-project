dcc587d1ed4207dc144b503af19448ec8eccc490
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:14:38 +0200

    Remove unnecessary function declarations

--- src/main/java/dev/kapiaszczyk/bookstore/library/author/AuthorRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/author/AuthorRepository.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    List<Author> findByFirstName(String firstName);
-    List<Author> findByLastName(String lastName);
-    List<Author> findByFirstNameAndLastName(String firstName, String lastName);
-    List<Author> findByCreditsBookTitle(String title);
+
+    List<Author> findAll();
+    List<Author> findByFirstNameOrLastName(String firstName, String lastName);
 }
