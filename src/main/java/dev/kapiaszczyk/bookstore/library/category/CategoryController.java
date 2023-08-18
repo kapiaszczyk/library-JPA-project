c548c13c7ac66dd6ffc33712ac4114abc0748f24
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:20:39 +0200

    Refactor CategoryController to use request parameters instead of path variables

--- src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryController.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/category/CategoryController.java
@@ -4,10 +4,7 @@ import dev.kapiaszczyk.bookstore.library.category.dto.CategoryStatisticsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,7 +18,7 @@ public class CategoryController {
         return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/find_by_name/{categoryName}")
+    @GetMapping("/find_by_name")
     public ResponseEntity<Category> getCategoryByName(@RequestParam("category_name") String categoryName) {
         return new ResponseEntity<>(categoryService.findByNameIgnoreCase(categoryName).orElse(null), HttpStatus.OK);
     }
