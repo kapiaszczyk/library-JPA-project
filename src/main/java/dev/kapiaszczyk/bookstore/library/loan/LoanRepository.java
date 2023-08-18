d9758a24f53eab5f8fba02570a115cced55730ee
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-18 11:23:14 +0200

    Add LoanRepository

--- src/main/java/dev/kapiaszczyk/bookstore/library/loan/LoanRepository.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/loan/LoanRepository.java
@@ -2,14 +2,30 @@ package dev.kapiaszczyk.bookstore.library.loan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-    List<Loan> findByAccountNumber(String accountNumber);
+//    // TODO: Create loan
+//    public Loan createLoan(Loan loan);
 
+//    // TODO: Delete loan
+//    public void deleteLoan(Long id);
+
+//    // TODO: Update loan
+//    public Loan updateLoan(Loan loan);
+
+    // TODO: Get loans by status
+    Iterable<Loan> findByStatus(Loan.Status status);
+
+    // TODO: Get loans by account number and status
     List<Loan> findByAccountNumberAndStatus(String accountNumber, Loan.Status status);
 
+    // TODO: Get loans by account id
+    List<Loan> findByAccountId(Long id);
 }
