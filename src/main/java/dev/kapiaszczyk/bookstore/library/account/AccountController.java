4ef914f91c1508a7107d0a6cd3d1dc8755b95647
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-08-22 10:18:47 +0200

    Refactor request handling methods not to raw use parametrized classes

--- src/main/java/dev/kapiaszczyk/bookstore/library/account/AccountController.java
+++ src/main/java/dev/kapiaszczyk/bookstore/library/account/AccountController.java
@@ -1,7 +1,6 @@
 package dev.kapiaszczyk.bookstore.library.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,29 +12,40 @@ public class AccountController {
     private AccountService accountService;
 
     @PostMapping("/add")
-    public ResponseEntity<Account> addAccount(@RequestBody Account account) {
-        return new ResponseEntity(accountService.addAccount(account), HttpStatus.OK);
+    public ResponseEntity<Object> addAccount(@RequestBody Account account) {
+        return ResponseEntity.ok(accountService.addAccount(account));
     }
 
     @RequestMapping("/get")
-    public ResponseEntity<Account> getAccount(@RequestParam("id") Long id) {
-        return new ResponseEntity(accountService.getAccount(id), HttpStatus.OK);
+    public ResponseEntity<Object> getAccount(@RequestParam("id") Long id) {
+        return ResponseEntity.ok(accountService.getAccount(id));
+    }
+
+    @RequestMapping("/getByLibraryUserId")
+    public ResponseEntity<Object> getAccountByLibraryUserId(@RequestParam("id") Long id) {
+        return ResponseEntity.ok(accountService.getAccountByLibraryUserId(id));
+    }
+
+    @RequestMapping("/getByNumber")
+    public ResponseEntity<Object> getAccountByNumber(@RequestParam("number") String number) {
+        return ResponseEntity.ok(accountService.getAccountByNumber(number));
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity deleteAccount(@RequestParam("id") Long id) {
         accountService.deleteAccount(id);
-        return new ResponseEntity(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
+    // TODO: Fix this
     @PutMapping("/update")
     public ResponseEntity updateAccount(@RequestBody Account account) {
-        return new ResponseEntity(accountService.updateAccount(account), HttpStatus.OK);
+        return ResponseEntity.ok(accountService.updateAccount(account));
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Iterable<Account>> getAllAccounts() {
-        return new ResponseEntity(accountService.findAll(), HttpStatus.OK);
+    public ResponseEntity<Object> getAllAccounts() {
+        return ResponseEntity.ok(accountService.findAll());
     }
 
 }
